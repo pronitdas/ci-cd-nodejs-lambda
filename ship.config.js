075e69f062159f633884bd6b2b8cd2f93d042c57
pronit <pronit78@gmail.com>
2020-08-18 14:07:37 +0530

    fix: checkout issues

--- ship.config.js
+++ ship.config.js
@@ -5,10 +5,10 @@ module.exports = {
     exec('git config --global user.email "github-actions[bot]@users.noreply.github.com"');
     exec('git config --global user.name "github-actions[bot]"');
     exec('git fetch --all');
-    exec('git checkout test');
+    exec('git checkout test --');
     exec('git rebase origin/stage');
     exec('git push origin test');
-    exec('git checkout develop');
+    exec('git checkout develop --');
     exec('git rebase origin/test');
     exec('git push origin develop');
   },
